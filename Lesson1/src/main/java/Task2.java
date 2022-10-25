a6d282433fa868a865883bf85f45a4a50a2fba83
victoryiakarpichka <vkarpichka@gmail.com>
2022-10-25 11:31:15 +0200

    решение домашнего задания

--- Lesson1/src/main/java/Task2.java
+++ Lesson1/src/main/java/Task2.java
@@ -1,18 +1,13 @@
 public class Task2 {
     public static void main(String[] args) {
-        String[] strings = new String[3];
-        strings[0] = "4";
-        strings[1] = "-11";
-        strings[2] = "50";
         int count = 0;
-        for (String string : strings) {
-            int x = Integer.parseInt(string);
+        for (String arg : args) {
+            int x = Integer.parseInt(arg);
             if (x > 0) {
                 count++;
-
             }
         }
-        System.out.println(count);
+        System.out.println("количество положительных чисел = " + count);
     }
 }
 
