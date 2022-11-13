5b7c88fc365d7262151ee339b1f1750b0995f51a
victoryiakarpichka <vkarpichka@gmail.com>
2022-11-13 18:49:52 +0100

    первые три задачи Lesson5 исправления

--- Lesson1/src/main/java/Task2.java
+++ Lesson1/src/main/java/Task2.java
@@ -1,13 +1,2 @@
-public class Task2 {
-    public static void main(String[] args) {
-        int count = 0;
-        for (String arg : args) {
-            int x = Integer.parseInt(arg);
-            if (x > 0) {
-                count++;
-            }
-        }
-        System.out.println("количество положительных чисел = " + count);
-    }
-}
+//
 
--- Lesson5/src/main/java/Task3.java
+++ Lesson5/src/main/java/Task3.java
@@ -4,13 +4,19 @@
 //        "5 - это положительное число, количество цифр = 1"
 public class Task3 {
     public static void main(String[] args) {
-        int number = 17;
-        if (number > 0) {
-            System.out.printf("%d - это положительное число, количество цифр = %s%n", number, (int) (Math.log10(number) + 1));
-        } else if (number < 0) {
-            System.out.printf("%d - это отрицательное число, количество цифр = %s%n", number, (int) (Math.log10(-number) + 1));
+        definitionOfNamber();
+    }
+
+    private static void definitionOfNamber() {
+        int a = 0;
+        if (a > 0) {
+            System.out.printf("%d - это положительное число, количество цифр = %s%n", a, (int) (Math.log10(a) + 1));
+        } else if (a < 0) {
+            System.out.printf("%d - это отрицательное число, количество цифр = %s%n", a, (int) (Math.log10(-a) + 1));
         } else {
             System.out.println("Число 0 состоит из 1 цифры и не является ни положительным, ни отрицательным");
         }
+        return;
     }
+
 }
