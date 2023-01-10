1e1178597cfa3658c1f87998f028be83fce09555
victoryiakarpichka <vkarpichka@gmail.com>
2023-01-10 14:48:07 +0100

    update

--- Lesson17/src/main/java/com/tms/services/Main.java
+++ Lesson17/src/main/java/com/tms/services/Main.java
@@ -5,8 +5,9 @@ import com.tms.models.Shop;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
+
+import static java.util.Comparator.comparingInt;
 
 
 public class Main {
@@ -19,12 +20,7 @@ public class Main {
 
         List<Product> listProduct = new ArrayList<>(shop.getProducts());
         System.out.println(shop.getProducts());
-        Collections.sort(listProduct, new Comparator<Product>() {
-            @Override
-            public int compare(Product o1, Product o2) {
-                return Integer.compare(o1.getPrice(), o2.getPrice());
-            }
-        });
+        Collections.sort(listProduct, comparingInt(Product::getPrice));
         System.out.println(listProduct);
         System.out.println(shop.deleteProduct(new Product(123L, "car", 10)));
         System.out.println(new ArrayList<>(shop.getProducts()));
--- Lesson18/src/main/java/by/tms/models/OneMethod.java
+++ Lesson18/src/main/java/by/tms/models/OneMethod.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 public class OneMethod {
     private Scanner console = new Scanner(System.in);
 
+    void chooseNumberForOperation() {
+        int number = 0;
+        if (console.nextInt() == 1) {
+            number = 1;
+        } else if (console.nextInt() == 2) {
+            number = 2;
+        }
+        switch (number) {
+            case 1 -> reversedString();
+            case 2 -> factorial();
+            default -> System.out.println("Other Method");
+        }
+    }
+
+    public String reversedString() {
+        return "text";
+    }
+
+    public int factorial() {
+        return 0;
+    }
 
     @FunctionalInterface
     public interface OperationAware<T> {
